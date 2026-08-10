33859f0cf2ea39f6d72affa517911569bd5d6136
kazi-hafiz-md-asad <kaziasadresearch@gmail.com>
2026-08-11 05:36:07 +0600

    Delete site-config.js

--- site-config.js
+++
@@ -1,9 +0,0 @@
-window.SITE_CONFIG = {
-  // CV: place Kazi_Hafiz_Md_Asad_CV.pdf in assets/pdf/ and change to true.
-  showCV: true,
-  cvUrl: "assets/pdf/Kazi_Hafiz_Md_Asad_CV.pdf",
-
-  // Photo: place Asad_1.png in assets/img/ and change to true.
-  showProfilePhoto: true,
-  profilePhotoUrl: "assets/img/Asad_1.png"
-};
